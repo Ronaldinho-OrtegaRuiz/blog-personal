0f9638cb4a70c3195df75fff83e2c50761140489
Ronaldinho-OrtegaRuiz <rortegar01@libertadores.edu.co>
2025-11-24 10:11:56 -0500

    fix: Actualizar titulo del blog en metadata.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "/blog/ - Blog Personal",
+  title: "RNDchan - Blog Personal",
   description: "Un blog sobre tecnología, educación, aprendizaje y más",
 };
 
